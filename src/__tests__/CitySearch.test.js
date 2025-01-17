3aeb7f82083e28dd779d63a369ca69b73bd91110
hand1ov1eternity <absurdism@duck.com>
2025-01-17 23:09:16 +0100

    write a test to verify  that the list of suggestions is correctly rendered

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -42,4 +42,35 @@ describe('<CitySearch /> component', () => {
       // eslint-disable-next-line no-undef
       expect(suggestionList).toHaveClass('suggestions');
     });
+
+    // eslint-disable-next-line no-undef
+    test('updates list of suggestions correctly when user types in city textbox', async () => {
+        const user = userEvent.setup();
+        // eslint-disable-next-line no-undef
+        const allEvents = await getEvents();
+        // eslint-disable-next-line no-undef
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    
+    
+        // user types "Berlin" in city textbox
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+    
+    
+        // filter allLocations to locations matching "Berlin"
+        const suggestions = allLocations? allLocations.filter((location) => {
+          return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+        }): [];
+    
+    
+        // get all <li> elements inside the suggestion list
+        const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+        // eslint-disable-next-line no-undef
+        expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i += 1) {
+          // eslint-disable-next-line no-undef
+          expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+        }
+      });
   });

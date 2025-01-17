869c80c231ce0bf2772d384796f9571cf2ec992b
hand1ov1eternity <absurdism@duck.com>
2025-01-17 17:51:09 +0100

    disable no-unused-vars

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 
+// eslint-disable-next-line no-undef
 describe('<EventList /> component', () => {
+    // eslint-disable-next-line no-undef
     test('renders correct number of events', () => {
         const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+        // eslint-disable-next-line no-undef
         expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
       });
 });
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,4 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import Event from './Event';
 
 
 const EventList = () => {

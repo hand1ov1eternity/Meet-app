1727d7cdcd047d458c0024a06995ae8cf90bb507
hand1ov1eternity <absurdism@duck.com>
2025-01-18 15:04:46 +0100

    Updating “EventList.test.js” to Use Mock Data

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
+import { getEvents } from '../api';
 
 // eslint-disable-next-line no-undef
 describe('<EventList /> component', () => {
@@ -18,11 +19,12 @@ describe('<EventList /> component', () => {
   });
  
   // eslint-disable-next-line no-undef
-  test('renders correct number of events', () => {
-    EventListComponent.rerender(<EventList events={
-      [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-    } />);
+  test('renders correct number of events', async () => {
     // eslint-disable-next-line no-undef
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    const allEvents = await getEvents(); 
+    EventListComponent.rerender(<EventList events={allEvents} />);
+// eslint-disable-next-line no-undef
+expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
- });
+  });
+ 

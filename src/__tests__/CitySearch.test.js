6cb0ce43274f5482c53dcafba2c1072af05b4fe9
hand1ov1eternity <absurdism@duck.com>
2025-01-20 11:28:39 +0100

    create <CitySearch /> integration test

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,80 +1,59 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { render } from '@testing-library/react';
-// eslint-disable-next-line no-unused-vars
+import { render, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
+import App from '../App';
 import { extractLocations, getEvents } from "../api";
 
-// eslint-disable-next-line no-undef
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
-    // eslint-disable-next-line no-undef
     beforeEach(() => {
       CitySearchComponent = render(<CitySearch />);
     });
-    // eslint-disable-next-line no-undef
+    
     test('renders text input', () => {
       const cityTextBox = CitySearchComponent.queryByRole('textbox');
-      // eslint-disable-next-line no-undef
       expect(cityTextBox).toBeInTheDocument();
-      // eslint-disable-next-line no-undef
       expect(cityTextBox).toHaveClass('city');
     });
   
-  
-    // eslint-disable-next-line no-undef
     test('suggestions list is hidden by default', () => {
       const suggestionList = CitySearchComponent.queryByRole('list');
-      // eslint-disable-next-line no-undef
       expect(suggestionList).not.toBeInTheDocument();
     });
   
-  
-    // eslint-disable-next-line no-undef
     test('renders a list of suggestions when city textbox gains focus', async () => {
       const user = userEvent.setup();
       const cityTextBox = CitySearchComponent.queryByRole('textbox');
       await user.click(cityTextBox);
       const suggestionList = CitySearchComponent.queryByRole('list');
-      // eslint-disable-next-line no-undef
       expect(suggestionList).toBeInTheDocument();
-      // eslint-disable-next-line no-undef
       expect(suggestionList).toHaveClass('suggestions');
     });
 
-    // eslint-disable-next-line no-undef
     test('updates list of suggestions correctly when user types in city textbox', async () => {
         const user = userEvent.setup();
-        // eslint-disable-next-line no-undef
         const allEvents = await getEvents();
-        // eslint-disable-next-line no-undef
         const allLocations = extractLocations(allEvents);
         CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
     
-    
-        // user types "Berlin" in city textbox
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         await user.type(cityTextBox, "Berlin");
-    
-    
-        // filter allLocations to locations matching "Berlin"
+
         const suggestions = allLocations? allLocations.filter((location) => {
           return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
         }): [];
-    
-    
-        // get all <li> elements inside the suggestion list
+
         const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
-        // eslint-disable-next-line no-undef
         expect(suggestionListItems).toHaveLength(suggestions.length + 1);
         for (let i = 0; i < suggestions.length; i += 1) {
-          // eslint-disable-next-line no-undef
           expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
         }
       });
 
-      // eslint-disable-next-line no-undef
       test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
         const user = userEvent.setup();
         const allEvents = await getEvents(); 
@@ -88,12 +67,30 @@ describe('<CitySearch /> component', () => {
     
         // the suggestion's textContent look like this: "Berlin, Germany"
         const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
-    
-    
         await user.click(BerlinGermanySuggestion);
-    
-    
-        // eslint-disable-next-line no-undef
         expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
       });
+  });
+
+  describe('<CitySearch /> integration', () => {
+
+    test('renders suggestions list when the app is rendered.', async () => {
+      const user = userEvent.setup();
+      const AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+   
+   
+      const CitySearchDOM = AppDOM.querySelector('#city-search');
+      const cityTextBox = within(CitySearchDOM).queryByRole('textbox');
+      await user.click(cityTextBox);
+   
+   
+      const allEvents = await getEvents();
+      const allLocations = extractLocations(allEvents);
+   
+   
+      const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+      expect(suggestionListItems.length).toBe(allLocations.length + 1);
+   });
+
   });

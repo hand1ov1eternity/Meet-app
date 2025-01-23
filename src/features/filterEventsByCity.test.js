092ce4a34166fe5a985e7985bfa6411479a948df
hand1ov1eternity <absurdism@duck.com>
2025-01-23 11:51:28 +0100

    testing scenario 2

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -5,10 +5,14 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
 import { getEvents } from '../mock-data';
+import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, (test) => {
+ 
+   //Scenario 1
+
   test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
     let AppComponent; 
 
@@ -31,39 +35,55 @@ defineFeature(feature, (test) => {
     });
   });
 
+  //Scenario 2
+  
   test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
     given('the main page is open', () => {
-      
+      let AppComponent;
+      given('the main page is open', () => {
+        AppComponent = render(<App />);
+      });
     });
 
     when('user starts typing in the city textbox', () => {
-      
+      let CitySearchDOM;
+      when('user starts typing in the city textbox', async () => {
+        const user = userEvent.setup();
+        const AppDOM = AppComponent.container.firstChild;
+        CitySearchDOM = AppDOM.querySelector('#city-search');
+        const citySearchInput = within(CitySearchDOM).queryByRole('textbox');  
+        await user.type(citySearchInput, "Berlin");
+      });
     });
 
-    then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-     
+    then('the user should receive a list of cities (suggestions) that match what they’ve typed', async () => {
+      const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem'); 
+      expect(suggestionListItems).toHaveLength(2);
     });
-  });
+});
 
-  test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
+//Scenario 3
+
+test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
     given('user was typing “Berlin” in the city textbox', () => {
-    
+        // setup code
     });
 
     and('the list of suggested cities is showing', () => {
-      
+        // setup code
     });
 
     when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-      
+        // action code
     });
 
     then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-      
+        // assertion code
     });
 
     and('the user should receive a list of upcoming events in that city', () => {
-     
+        // additional assertions
     });
-  });
+});
+
 });

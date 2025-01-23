0a0780f4f45f5fecf3b7396199ef1c0bafc745b4
hand1ov1eternity <absurdism@duck.com>
2025-01-23 11:14:33 +0100

    testing scenario 1

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,58 +1,69 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+import React from 'react';
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render, within, waitFor } from '@testing-library/react';
+import App from '../App';
+import { getEvents } from '../mock-data';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
-defineFeature(feature, test => {
+defineFeature(feature, (test) => {
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
+    let AppComponent; 
 
-    test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
-        given('user hasn’t searched for any city', () => {
-
-        });
-
-        when('the user opens the app', () => {
-
-        });
-
-        then('the user should see the list of all upcoming events.', () => {
-
-        });
+    given('user hasn’t searched for any city', () => {
+      
     });
 
-    test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
-        given('the main page is open', () => {
-
-        });
-
-        when('user starts typing in the city textbox', () => {
-
-        });
-
-        then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
-        });
+    when('the user opens the app', () => {
+      AppComponent = render(<App />); 
     });
 
-    test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
-        given('user was typing “Berlin” in the city textbox', () => {
+    then('the user should see the list of all upcoming events.', async () => {
+      const AppDOM = AppComponent.container.firstChild;
+      const EventListDOM = AppDOM.querySelector('#event-list');
 
-        });
+      await waitFor(() => {
+        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(EventListItems.length).toBe(32);
+      });
+    });
+  });
 
-        and('the list of suggested cities is showing', () => {
-
-        });
-
-        when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-
-        });
-
-        then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-
-        });
-
-        and('the user should receive a list of upcoming events in that city', () => {
-
-        });
+  test('User should see a list of suggestions when they search for a city.', ({ given, when, then }) => {
+    given('the main page is open', () => {
+      
     });
 
+    when('user starts typing in the city textbox', () => {
+      
+    });
+
+    then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+     
+    });
+  });
+
+  test('User can select a city from the suggested list.', ({ given, and, when, then }) => {
+    given('user was typing “Berlin” in the city textbox', () => {
+    
+    });
+
+    and('the list of suggested cities is showing', () => {
+      
+    });
+
+    when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+      
+    });
+
+    then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+      
+    });
+
+    and('the user should receive a list of upcoming events in that city', () => {
+     
+    });
+  });
 });

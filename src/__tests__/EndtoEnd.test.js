d86077e8bdceccd5c21c223b9806104ebb603d13
hand1ov1eternity <absurdism@duck.com>
2025-01-24 21:33:52 +0100

    updated puppetter, still testing test.js file

--- src/__tests__/EndtoEnd.test.js
+++ src/__tests__/EndtoEnd.test.js
@@ -1,30 +1,31 @@
 /* eslint-disable no-undef */
 import puppeteer from "puppeteer";
 
-describe('show/hide an event details', () => {
+describe("show/hide an event details", () => {
+  let browser;
+  let page;
 
-    let browser;
-    let page;
-    beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 250,
-            timeout: 0
-        });
-        page = await browser.newPage();
-        await page.goto('http://localhost:5173/');
-        await page.waitForSelector('.event');
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      timeout: 30000, // 30 seconds
     });
+    page = await browser.newPage();
+    await page.goto("http://localhost:5173/");
+    await page.waitForSelector(".event");
+  });
 
-    afterAll(() => {
-        browser.close();
-    });
+  afterAll(async () => {
+    if (page) await page.close(); // Close the page
+    if (browser) await browser.close(); // Close the browser
+  });
 
-    test('User can expand an event to see its details', async () => {
-        await page.waitForSelector('.event');
-        await page.click('.event button'); // Click on the button instead of .details-btn
-        
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeDefined(); // Checks that the details are displayed
-    });
+  test("User can expand an event to see its details", async () => {
+    await page.waitForSelector(".event");
+    await page.click(".event button"); // Click on the button
+
+    const eventDetails = await page.$(".event .details");
+    expect(eventDetails).toBeDefined(); // Ensure details are displayed
+  });
 });

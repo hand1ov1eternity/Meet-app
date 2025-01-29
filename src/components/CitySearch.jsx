b30f7313dc660077fa47f8c562f75733ee467fdb
hand1ov1eternity <absurdism@duck.com>
2025-01-29 10:25:57 +0100

    add styling to the alert box, call InfoAlert component in CitySearch.jsx

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
@@ -19,6 +20,14 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
       : [];
     setQuery(value);
     setSuggestions(filteredLocations);
+
+    let infoText;
+    if (filteredLocations.length === 0) {
+      infoText = "We can not find the city you are looking for. Please try another city"
+    } else {
+      infoText = ""
+    }
+    setInfoAlert(infoText);
   };
 
   const handleItemClicked = (event) => {
@@ -26,6 +35,7 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
     setQuery(value);
     setShowSuggestions(false); // to hide the list
     setCurrentCity(value);
+    setInfoAlert("")
   };
 
   useEffect(() => {

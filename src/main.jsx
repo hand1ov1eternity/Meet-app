1de79a7692e765a8b2f258a95a1df6b1c9b4b1ee
hand1ov1eternity <absurdism@duck.com>
2025-02-03 12:59:33 +0100

    Import the service worker and register it

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-atatus.config('9ec039446b0541188b6baeb9051973e3').install();
+//atatus.config('9ec039446b0541188b6baeb9051973e3').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+serviceWorkerRegistration.register();
